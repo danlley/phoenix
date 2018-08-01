6fe881343177b2236530887be057ea4e7d0a9ce8
danlley <danlley@126.com>
2018-08-01 13:35:06 +0800

    套餐详情图片管理

---
+++ phoenix-biz-service-impl/src/main/java/com/myteay/phoenix/biz/service/impl/admin/controller/PxGoodsPackagesImageController.java
@@ -0,0 +1,112 @@
+/**
+ * Myteay.com Inc.
+ * Copyright (c) 2015-2018 All Rights Reserved.
+ */
+package com.myteay.phoenix.biz.service.impl.admin.controller;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myteay.phoenix.biz.service.impl.MtServiceResult;
+import com.myteay.phoenix.common.util.enums.MtOperateExResultEnum;
+import com.myteay.phoenix.common.util.enums.MtOperateResultEnum;
+import com.myteay.phoenix.core.model.MtOperateResult;
+import com.myteay.phoenix.core.model.manage.PxGoodsPackagesImageModel;
+import com.myteay.phoenix.core.service.manage.component.PxCommonManageComponent;
+import com.myteay.phoenix.core.service.manage.component.PxGoodsPackagesImageComponent;
+
+/**
+ * 套餐详情图片管理
+ * 
+ * @author min.weixm
+ * @version $Id: PxGoodsPackagesImageController.java, v 0.1 Aug 1, 2018 1:30:25 PM min.weixm Exp $
+ */
+@RestController
+@RequestMapping(value = "myteay/api/phoenix/admin/manage/pkgs/image")
+public class PxGoodsPackagesImageController {
+
+    /** 日志 */
+    public static final Logger            logger = Logger.getLogger(PxGoodsPackagesImageController.class);
+
+    /** 后台一般性简单业务管理组件 */
+    @Autowired
+    private PxCommonManageComponent       pxCommonManageComponent;
+
+    /** 套餐详情图片管理 */
+    @Autowired
+    private PxGoodsPackagesImageComponent pxGoodsPackagesImageComponent;
+
+    /**
+     * 通过商品摘要ID查询店铺下的所有套餐详情图片列表
+     * 
+     * @param shopId
+     * @return
+     */
+    @RequestMapping(value = "/list/goods/{goodsId}", method = { RequestMethod.GET })
+    public MtServiceResult<List<PxGoodsPackagesImageModel>> queryGoodsByShopId(@PathVariable String goodsId) {
+        MtServiceResult<List<PxGoodsPackagesImageModel>> result = null;
+
+        MtOperateResult<List<PxGoodsPackagesImageModel>> componentResult = null;
+        try {
+            componentResult = pxCommonManageComponent.queryPackagesImageListByGoodsId(goodsId);
+            result = new MtServiceResult<>(componentResult.getOperateResult(), componentResult.getOperateExResult());
+            result.setResult(componentResult.getResult());
+        } catch (Exception e) {
+            logger.warn("查询套餐详情图片信息发生未知异常 " + e.getMessage(), e);
+            result = new MtServiceResult<>(MtOperateResultEnum.CAMP_OPERATE_UNKONW, MtOperateExResultEnum.CAMP_UNKNOW_ERR);
+        }
+
+        return result;
+    }
+
+    /**
+     * 套餐详情图片管理服务（增、删、改、单条查询）
+     * 
+     * @param pxShopModel
+     * @return
+     */
+    @RequestMapping(value = "/manage", method = { RequestMethod.POST })
+    public MtServiceResult<PxGoodsPackagesImageModel> manageGoods(@RequestBody PxGoodsPackagesImageModel pxGoodsPackagesImageModel) {
+
+        if (logger.isInfoEnabled()) {
+            logger.info("开始保存套餐详情图片信息 pxGoodsPackagesImageModel=" + pxGoodsPackagesImageModel);
+        }
+        MtServiceResult<PxGoodsPackagesImageModel> result = null;
+        try {
+            MtOperateResult<PxGoodsPackagesImageModel> innerResult = pxGoodsPackagesImageComponent.manageGoodsPackagesImage(pxGoodsPackagesImageModel);
+            result = new MtServiceResult<>(innerResult.getOperateResult(), innerResult.getOperateExResult());
+            result.setResult(innerResult.getResult());
+        } catch (Exception e) {
+            logger.warn("保存套餐详情图片信息发生异常" + e.getMessage(), e);
+            result = new MtServiceResult<>(MtOperateResultEnum.CAMP_OPERATE_UNKONW, MtOperateExResultEnum.CAMP_UNKNOW_ERR);
+        }
+
+        return result;
+    }
+
+    /**
+     * Setter method for property <tt>pxCommonManageComponent</tt>.
+     * 
+     * @param pxCommonManageComponent value to be assigned to property pxCommonManageComponent
+     */
+    public void setPxCommonManageComponent(PxCommonManageComponent pxCommonManageComponent) {
+        this.pxCommonManageComponent = pxCommonManageComponent;
+    }
+
+    /**
+     * Setter method for property <tt>pxGoodsPackagesImageComponent</tt>.
+     * 
+     * @param pxGoodsPackagesImageComponent value to be assigned to property pxGoodsPackagesImageComponent
+     */
+    public void setPxGoodsPackagesImageComponent(PxGoodsPackagesImageComponent pxGoodsPackagesImageComponent) {
+        this.pxGoodsPackagesImageComponent = pxGoodsPackagesImageComponent;
+    }
+
+}
