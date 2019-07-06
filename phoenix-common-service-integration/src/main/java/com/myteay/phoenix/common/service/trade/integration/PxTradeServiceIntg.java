347483c802f79df048de68a4f6d2e0e238df9944
danlley <danlley@126.com>
2019-07-06 22:23:34 +0800

    交易平台对接

---
+++ phoenix-common-service-integration/src/main/java/com/myteay/phoenix/common/service/trade/integration/PxTradeServiceIntg.java
@@ -0,0 +1,25 @@
+/**
+ * GanguTianCan.com Inc.
+ * Copyright (c) 2005-2019 All Rights Reserved.
+ */
+package com.myteay.phoenix.common.service.trade.integration;
+
+import com.myteay.phoenix.common.util.enums.PxOrderStatusEnum;
+import com.myteay.phoenix.common.util.enums.PxPayTypeEnum;
+import com.myteay.phoenix.core.model.MtOperateResult;
+import com.myteay.phoenix.core.model.PxGoodsOrderModel;
+import com.myteay.phoenix.core.model.camp.CampCashierModel;
+import com.myteay.phoenix.core.model.camp.CampShopPrizeOutModel;
+
+/**
+ * 
+ * @author min.weixm
+ * @version $Id: TradeServiceIntg.java, v 0.1 Jul 6, 2019 9:28:20 PM min.weixm Exp $
+ */
+public interface PxTradeServiceIntg {
+
+    public MtOperateResult<String> modifyGoodsOrderOut(String orderNo, PxPayTypeEnum pxPayTypeEnum, PxOrderStatusEnum pxOrderStatusEnum,
+                                                       CampShopPrizeOutModel campShopPrizeOutModel);
+
+    public MtOperateResult<CampCashierModel> createGoodsOrderOut(PxGoodsOrderModel pxGoodsOrderModel);
+}
---
+++ phoenix-common-service-integration/src/main/java/com/myteay/phoenix/common/service/trade/integration/impl/PxTradeServiceIntgImpl.java
@@ -0,0 +1,48 @@
+/**
+ * GanguTianCan.com Inc.
+ * Copyright (c) 2005-2019 All Rights Reserved.
+ */
+package com.myteay.phoenix.common.service.trade.integration.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.myteay.phoenix.common.service.trade.integration.PxTradeServiceIntg;
+import com.myteay.phoenix.common.util.HttpClientUtil;
+import com.myteay.phoenix.common.util.enums.PxOrderStatusEnum;
+import com.myteay.phoenix.common.util.enums.PxPayTypeEnum;
+import com.myteay.phoenix.core.model.MtOperateResult;
+import com.myteay.phoenix.core.model.PxGoodsOrderModel;
+import com.myteay.phoenix.core.model.camp.CampCashierModel;
+import com.myteay.phoenix.core.model.camp.CampShopPrizeOutModel;
+
+/**
+ * 
+ * @author min.weixm
+ * @version $Id: PxTradeServiceIntgImpl.java, v 0.1 Jul 6, 2019 9:29:16 PM min.weixm Exp $
+ */
+public class PxTradeServiceIntgImpl implements PxTradeServiceIntg {
+
+    /** 
+     * @see com.myteay.phoenix.common.service.trade.integration.PxTradeServiceIntg#modifyGoodsOrderOut(java.lang.String, com.myteay.phoenix.common.util.enums.PxPayTypeEnum, com.myteay.phoenix.common.util.enums.PxOrderStatusEnum, com.myteay.phoenix.core.model.camp.CampShopPrizeOutModel)
+     */
+    @Override
+    public MtOperateResult<String> modifyGoodsOrderOut(String orderNo, PxPayTypeEnum pxPayTypeEnum, PxOrderStatusEnum pxOrderStatusEnum,
+                                                       CampShopPrizeOutModel campShopPrizeOutModel) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** 
+     * @see com.myteay.phoenix.common.service.trade.integration.PxTradeServiceIntg#createGoodsOrderOut(com.myteay.phoenix.core.model.PxGoodsOrderModel)
+     */
+    @Override
+    public MtOperateResult<CampCashierModel> createGoodsOrderOut(PxGoodsOrderModel pxGoodsOrderModel) {
+        String url = "http://192.168.0.103:40031/myteay/api/phoenix/cashier/manage/base/order/";
+        String result = HttpClientUtil.insureResponsePost(url, JSON.toJSONString(pxGoodsOrderModel));
+        MtOperateResult<CampCashierModel> obj = (MtOperateResult<CampCashierModel>) JSON.parseObject(result,
+            new TypeReference<MtOperateResult<CampCashierModel>>() {
+            });
+        return obj;
+    }
+
+}
